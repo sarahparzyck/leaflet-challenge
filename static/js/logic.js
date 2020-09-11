96b4464a3eacaae8ec707c5439846afe738434ad
Sarah Parzyck <sarahparzyck@gmail.com>
2020-09-10 22:01:32 -0500

    final update

--- static/js/logic.js
+++ static/js/logic.js
@@ -44,11 +44,11 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes) {
     // Create the tile layer that will be the background of our map
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3Bhcnp5Y2siLCJhIjoiY2tlYWxiNnI4MDFveTJzcXliczMzYmxpZCJ9.bbmbV29NnHcng_ss1QRsgQ", {
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         maxZoom: 18,
         id: "mapbox/streets-v11",
-        
+        accessToken: API_KEY
     });   
 
     // Define a baseMaps object to hold our base layers
